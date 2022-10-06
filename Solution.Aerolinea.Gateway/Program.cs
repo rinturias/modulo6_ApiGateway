151fde2035c7beebbed3009d56dc3fb2d11a9441
Ronal Inturias <ronal.95.03@gmail.com>
2022-10-06 14:30:21 -0400

    se creo cors

--- Solution.Aerolinea.Gateway/Program.cs
+++ Solution.Aerolinea.Gateway/Program.cs
@@ -2,6 +2,7 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Solution.Aerolinea.Gateway.Aggregator;
 
+var MyAllowSpecificOrigins = "OriginsCors";
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -10,11 +11,23 @@ builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot()
             .AddTransientDefinedAggregator<DetalleAsientoPorVueloAggregator>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+});
+
+});
 
 var app = builder.Build();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseOcelot().Wait();
 
+//app.UseAuthorization();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
